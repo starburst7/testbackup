c3e9dee1080459e8d8f69663cb33c241070001f1
starburst7 <dutt.a@live.ca>
2018-11-28 07:46:34 -0500

    -added basic linux commands, -continuedArrayList

--- DataStructures/src/lists/MyArrayList.java
+++ DataStructures/src/lists/MyArrayList.java
@@ -1,7 +1,159 @@
 package lists;
 
+import java.util.Collection;
+import java.util.Iterator;
 import java.util.List;
+import java.util.ListIterator;
 
-public class MyArrayList implements List<E> {
+public class MyArrayList<E> implements List<E> {
 
+	private E[] array;
+	
+	@Override
+	public void add(int index, E element) {
+		
+		
+	}
+	
+	@Override
+	public E remove(int index) {
+		
+		return null;
+	}
+	
+	@Override
+	public int size() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+	
+	@Override
+	public void clear() {
+		
+		
+	}
+	
+	@Override
+	public boolean add(E e) {
+		
+		return false;
+	}
+	
+	@Override
+	public boolean remove(Object o) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean isEmpty() {
+		throw new UnsupportedOperationException();
+		
+	}
+
+	@Override
+	public boolean contains(Object o) {
+		throw new UnsupportedOperationException();
+		
+	}
+
+	@Override
+	public Iterator<E> iterator() {
+		throw new UnsupportedOperationException();
+		
+	}
+
+	@Override
+	public Object[] toArray() {
+		throw new UnsupportedOperationException();
+		
+	}
+
+	@Override
+	public <T> T[] toArray(T[] a) {
+		throw new UnsupportedOperationException();
+		
+	}
+
+	
+	@Override
+	public boolean containsAll(Collection<?> c) {
+		throw new UnsupportedOperationException();
+		
+	}
+
+	@Override
+	public boolean addAll(Collection<? extends E> c) {
+		throw new UnsupportedOperationException();
+		
+	}
+
+	@Override
+	public boolean addAll(int index, Collection<? extends E> c) {
+		throw new UnsupportedOperationException();
+		
+	}
+
+	@Override
+	public boolean removeAll(Collection<?> c) {
+		throw new UnsupportedOperationException();
+		
+	}
+
+	@Override
+	public boolean retainAll(Collection<?> c) {
+		throw new UnsupportedOperationException();
+		
+	}
+
+
+
+	@Override
+	public E get(int index) {
+		throw new UnsupportedOperationException();
+		
+	}
+
+	@Override
+	public E set(int index, E element) {
+		throw new UnsupportedOperationException();
+		
+	}
+
+	
+
+	
+
+	@Override
+	public int indexOf(Object o) {
+		throw new UnsupportedOperationException();
+		
+	}
+
+	@Override
+	public int lastIndexOf(Object o) {
+		throw new UnsupportedOperationException();
+		
+	}
+
+	@Override
+	public ListIterator<E> listIterator() {
+		throw new UnsupportedOperationException();
+	
+	}
+
+	@Override
+	public ListIterator<E> listIterator(int index) {
+		throw new UnsupportedOperationException();
+		
+	}
+
+	@Override
+	public List<E> subList(int fromIndex, int toIndex) {
+		throw new UnsupportedOperationException();
+		
+	}
+
+	
+	
 }

524fa190d131bea52a6985b902658a4736591acd
starburst7 <dutt.a@live.ca>
2018-12-05 13:11:12 -0500

    f

--- Basics/src/test/Test.java
+++ Basics/src/test/Test.java
@@ -12,48 +12,7 @@ public class Test {
 
 	public static void main(String[] args) {
 		
-		Scanner sc = null;
-		PrintWriter pw=null;
-		
-		File fIn = new File("C:\\Users\\2flare\\Desktop\\bookmdata.txt");
-		File fOut = new File("C:\\Users\\2flare\\Desktop\\bookmdataUpd.txt");
-try
-	{
-			sc = new Scanner(fIn);
-			pw = new PrintWriter(new FileOutputStream(fOut));
-			
-			String line="";
-			
-while(sc.hasNextLine()==true)
-   {			
-		line=sc.nextLine();
 	
-		if(line.contains("Chapter"))
-            pw.println(line);
-		
-		else {
-			
-			line=line.replace(".","");
-			pw.println(line);
-		}
-		pw.flush();
-	}
-
-
-}
-		
-		
-		
-		catch(FileNotFoundException e)
-		{
-			System.out.print("FNFE");
-		}
- catch(NoSuchElementException f)
- {
-pw.close();
- sc.close();
-	 
- }
 	
 	
 	}

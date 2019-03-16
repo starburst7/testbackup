1cf67afae23a87876c99b89baba59d139d1612ea
starburst7 <dutt.a@live.ca>
2019-03-16 19:22:35 -0400

    f

---
+++ DataStructures/src/webCrawler/WebCrawler.java
@@ -0,0 +1,32 @@
+package webCrawler;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+public class WebCrawler {
+
+    private Queue<String> queue;
+    private List<String> discoveredWebsitesList;
+    
+    public WebCrawler() {
+        this.queue=new LinkedList<String>();
+        this.discoveredWebsitesList=new ArrayList<String>();
+    }
+    public void discoverWeb(String root) {
+        this.queue.add(root);
+        this.discoveredWebsitesList.add(root);
+        
+        while (!queue.isEmpty()) {
+            
+            String temp= this.queue.remove();
+            String rawHtml = readURL(temp);
+        }
+    }
+    
+    public static void main(String[] args) {
+   
+    }
+
+}
